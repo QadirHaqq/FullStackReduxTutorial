edcdee59a60eb69f03d1555bb1b12fabd2b5994c
Qadir Haqq <qhaqq@amplify.com>
2017-03-03 14:13:40 -0500

    Just a commit

--- voting-server/test/reducer_spec.js
+++ voting-server/test/reducer_spec.js
@@ -58,20 +58,6 @@ describe('reducer', () => {
         }));
     });
 
-    it('can be used with reduce',() => {
-        const action = [
-        {type: SET_ENTRIES, entries: ['Trainspotting','28 Days Later']},
-        {type: NEXT},
-        {type: VOTE, entry: 'Trainspotting'},
-        {type: VOTE, entry: '28 Days Later'},
-        {type: VOTE, entry: 'Trainspotting'},
-        {type: NEXT}
-        ];
-        const finalState = actions.reduce(reducer, Map());
-
-        expect(finalState).to.equal(fromJS({
-            winner: 'Trainspotting'
-        }));
-    });
+    it(
 
 });
